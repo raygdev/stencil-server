30eb102b59089ba8bf4438234666bdbf4372cf85
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 12:56:10 -0400

    Remove comments in setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,31 +1,12 @@
 import { sequelize } from "../sequelize.config";
-import { User } from "../models/user-model";
+
 
 beforeAll(async () => {
     await sequelize.authenticate()
 })
 
-// beforeEach( async () => {
-//     await User.sync({ force: true })
-// })
-
 afterAll(async () => {
     await sequelize.close()
 })
 
-// beforeAll(async () => {
-//     // await sequelize.sync({ force: true })
-//     await sequelize.authenticate()
-//     const db = sequelize.getDatabaseName()
-//     // await User.sync({ force: true })
-//     if(db !== 'test-posts') {
-//         console.log('db name is production name exiting process')
-//         await sequelize.close()
-//         process.exit()
-//     }
-// })
-
-// afterAll(async () => {
-//     await sequelize.close()
-// })
 
