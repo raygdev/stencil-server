f8b7233ad788c23349cf4910b4ff0ce8759fd2f9
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 13:31:50 -0400

    Handle not authorized errors

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -1,15 +1,25 @@
 import jwt from 'jsonwebtoken'
 import { Request, Response, RequestHandler, NextFunction } from 'express'
 import { User } from '../models/user-model'
+import { NotAuthorizedError, NotFoundError } from '../errors'
 
 export const requireAuth: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.get('authorization')?.split(" ")[1]
 
-  if(!token) throw new Error("Not Authorized")
+  if(!token) throw new NotAuthorizedError()
 
-  const { id } = jwt.verify(token, process.env.TOKEN_SECRET!) as { id: string }
+   try {
+     const { id } = jwt.verify(token, process.env.TOKEN_SECRET!) as { id: string }
+   
+     const user = await User.findOne({ where: { id }})
 
-  const user = await User.findOne({ where: { id }})
-  req.user = user?.toJSON()
+     if(!user) throw new NotFoundError()
+     req.user = user.toJSON()
+
+   } catch(e) {
+    console.log("[AuthError]", e)
+    throw new NotAuthorizedError()
+   }
+
   next()
 }
