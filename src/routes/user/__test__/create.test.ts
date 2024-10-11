3a8da60776a057474c1f9b2c7f9d4422ee2e80a5
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 16:03:57 -0400

    Add tests for validation

--- src/routes/user/__test__/create.test.ts
+++ src/routes/user/__test__/create.test.ts
@@ -5,25 +5,85 @@ import { User } from "../../../models/user-model";
 
 describe('POST /api/users/create', () => {
 
-    beforeEach(async () => {
-        await User.sync({ force: true })
-    })
+   beforeEach((done) => {
+    User.sync({ force: true })
+     .then(() => done())
+     .catch(e => {
+      console.log(e)
+      done()
+     })
+   })
 
    const userInfo = {
     firstName: 'Some',
     lastName: 'Guy',
     email: 'some.guy@gmail.com',
-    password: 'test123'
+    password: 'Test123!'
    }
 
-   it('creates a new user', async () => {
+   it('fails on invalid email address', async () => {
+    const res = await request(app)
+      .post('/api/users/create')
+      .send({
+        ...userInfo,
+        email: ''
+      })
+      .expect(400)
+
+      expect(res.body.errors).toBeTruthy()
+      expect(Array.isArray(res.body.errors)).toBe(true)
+      expect(res.body.errors[0].field).toEqual('email')
+   })
+
+   it('fails on invalid first name', async () => {
+    const res = await request(app)
+      .post('/api/users/create')
+      .send({
+        ...userInfo,
+        firstName: ''
+      })
+      .expect(400)
+
+      expect(res.body.errors).toBeTruthy()
+      expect(res.body.errors[0].field).toEqual('firstName')
+   })
+
+   it('fails on invalid last name', async () => {
+    const res = await request(app)
+      .post('/api/users/create')
+      .send({
+        ...userInfo,
+        lastName: ''
+      })
+      .expect(400)
+
+      expect(res.body.errors).toBeTruthy()
+      expect(res.body.errors[0].field).toEqual('lastName')
+   })
+
+   it('fails on invalid password', async () => {
+    const res = await request(app)
+      .post('/api/users/create')
+      .send({
+        ...userInfo,
+        password: ''
+      })
+      .expect(400)
+
+      expect(res.body.errors).toBeTruthy()
+      expect(res.body.errors[0].field).toEqual('password')
+   })
+
+   it('creates a new user with valid properties', async () => {
     const req = await request(app)
       .post('/api/users/create')
       .send(userInfo)
-      .expect(200)
+      .expect(201)
+      // console.log(req.body.errors)
 
     expect(req.body.firstName).toEqual(userInfo.firstName)
     expect(req.body.lastName).toEqual(userInfo.lastName)
 
    })
+
 })
