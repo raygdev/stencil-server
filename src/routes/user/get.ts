6c853ef95f9161a0204186c54825c3556c302b4f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 13:34:38 -0400

    Use not found error

--- src/routes/user/get.ts
+++ src/routes/user/get.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { User } from "../../models/user-model";
+import { NotFoundError } from "../../errors";
 export const get = async (req: Request, res: Response) => {
   const user = await User.findOne({
     attributes: { exclude: ['password']},
     where: { id: req.user?.id },
-});
-
-  res.json(user?.toJSON());
+  });
+  
+  if(!user) throw new NotFoundError()
+  res.json(user.toJSON());
 };
