831f50f9e7caa440b4be0b0084a69073e671d429
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-12 07:24:13 -0400

    Test get route

--- src/routes/user/__test__/get.test.ts
+++ src/routes/user/__test__/get.test.ts
@@ -1,10 +1,58 @@
 import request from 'supertest'
 import { User } from '../../../models/user-model'
+import jwt from 'jsonwebtoken'
 import { app } from '../../../app'
 
 
 describe('GET /api/users/get', () => {
-    it.todo('fails when no authorization header is present')
-    it.todo('fails with invalid token')
-    it.todo('successfully responds with the requesting user\'s information')
+    beforeEach(async () => {
+        try {
+          await User.sync()
+        } catch (error) {  
+        }
+    })
+
+    afterEach(async () => {
+        try {
+          await User.drop()
+        } catch (e) {  
+        }
+    })
+    it('fails when no authorization header is present', async () => {
+        const res = await request(app)
+          .get('/api/users/get')
+          .send()
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized')
+          expect(res.body.firstName).not.toBeTruthy()
+    })
+    it('fails with invalid token', async () => {
+        const res = await request(app)
+          .get('/api/users/get')
+          .auth('123', { type: 'bearer'})
+          .send()
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized')
+          expect(res.body.firstName).not.toBeTruthy()
+    })
+    it('successfully responds with the requesting user\'s information', async () => {
+        const user = await createUser({
+            firstName: 'Test',
+            lastName: 'User',
+            email: 'test@test.com',
+            password: 'Test123!'
+        })
+
+        const { id } = user as unknown as { id: number}
+
+        const token = jwt.sign({ id }, process.env.TOKEN_SECRET!)
+
+        const res = await request(app)
+          .get('/api/users/get')
+          .auth(token, { type: 'bearer' })
+          .send()
+          .expect(200)
+    })
 })
