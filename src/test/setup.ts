838c0c331ddf5aec4808c7a24dcce6e1aff0b8b5
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 20:11:47 -0400

    Create global create for tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,20 @@
+import bcrypt from 'bcrypt'
 import { User } from "../models/user-model";
+import { ICreateUserRequest } from "../routes/user/create";
 import { sequelize } from "../sequelize.config";
+
+declare global {
+    var createUser: (user: ICreateUserRequest) => Promise<ICreateUserRequest>
+}
+
+global.createUser = async (user) => {
+    const newUser = await User.create({
+        ...user,
+        password: bcrypt.hashSync(user.password, 10)
+    })
+    await newUser.save()
+    return newUser.toJSON()
+}
 
 
 beforeAll(async () => {
