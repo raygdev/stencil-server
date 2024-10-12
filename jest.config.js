ae58f39e12ab2991b3fac8efca51bd4dc3c96e7d
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-11 20:08:29 -0400

    Point to test setup

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports =  {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ["./src"],
-    // setupFilesAfterEnv: ['./src/test/jest-setup.ts']
+    setupFilesAfterEnv: ['./src/test/setup.ts']
 }
