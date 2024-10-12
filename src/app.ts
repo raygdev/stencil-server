2fea810f5d3800bd44d37a426aeae6af2be88d37
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-12 18:09:17 -0400

    Add post routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { config } from "dotenv";
 import { userRouter } from "./routes/user";
 import { signinRouter } from './routes/auth/signin';
+import { postRouter } from './routes/post';
 import { errorHandler } from './middleware';
 import { NotFoundError } from './errors/not-fround-error';
 config({path: "./.env"})
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(signinRouter)
 app.use(userRouter)
+app.use(postRouter)
 
 app.all('*', () => {
   throw new NotFoundError()
