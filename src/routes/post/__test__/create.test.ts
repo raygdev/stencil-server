d75134280713e426ae27b0e03bf140b7e0ee6018
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-13 09:13:26 -0400

    Add create tests

--- src/routes/post/__test__/create.test.ts
+++ src/routes/post/__test__/create.test.ts
@@ -1,16 +1,77 @@
 import request from 'supertest'
 import { app } from '../../../app';
-import { User } from "../../../models/user-model";
-import { Post } from "../../../models/posts-model";
-
-
-
 describe('POST /api/posts/new', () => {
-    it.todo('should fail with no authentication header')
-    it.todo('should fail on invalid token')
-    it.todo('should fail with an empty note')
-    it.todo('should fail if not a string')
-    it.todo('should successfully create a post')
-    it.todo('should should successfully sanitize html entities')
-    it.todo('')
+
+    it('should fail with no authentication header', async () => {
+        const res = await request(app)
+          .post('/api/posts/new')
+          .send({ note: 'something' })
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized');
+    })
+
+    it('should fail on invalid token', async () => {
+        const res = await request(app)
+          .post('/api/posts/new')
+          .auth('123', { type: 'bearer' })
+          .send({ note: 'something' })
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized');
+    })
+
+    it('should fail with an empty note', async () => {
+        const res = await request(app)
+          .post('/api/posts/new')
+          .send({ note: '' })
+          .expect(400)
+
+          expect(res.body.errors[0].message).toEqual('note must be provided');
+    })
+
+    it('should fail if not a string', async () => {
+        const res = await request(app)
+          .post('/api/posts/new')
+          .send({ note: { other: 'something' } })
+          .expect(400)
+
+          expect(res.body.errors[0].message).toEqual('Notes must be a string');
+    })
+
+    it('should successfully create a post', async () => {
+        const { token, id } = await createUser()
+        const res = await request(app)
+          .post('/api/posts/new')
+          .auth(token, { type: 'bearer' })
+          .send({ note: 'something' })
+          .expect(200)
+
+          expect(res.body.note).toEqual("something");
+          expect(res.body.userId).toEqual(id)
+    })
+
+    it('should successfully sanitize html entities', async () => {
+        const { token, id } = await createUser()
+        const res = await request(app)
+          .post('/api/posts/new')
+          .auth(token, { type: 'bearer' })
+          .send({ note: '<>&' })
+          .expect(200)
+
+          expect(res.body.note).toEqual("&lt;&gt;&amp;");
+          expect(res.body.userId).toEqual(id) 
+    })
+
+    it('should successfully trim whitespace', async () => {
+        const { token, id } = await createUser()
+        const res = await request(app)
+          .post('/api/posts/new')
+          .auth(token, { type: 'bearer' })
+          .send({ note: '    something     ' })
+          .expect(200)
+
+          expect(res.body.note).toEqual("something");
+          expect(res.body.userId).toEqual(id)
+    })
 })
