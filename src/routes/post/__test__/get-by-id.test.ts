d6d5f8c157ff2ce26b52ae28e4f2a8be93785b15
raygdev <101662915+raygdev@users.noreply.github.com>
2024-10-13 10:09:41 -0400

    Create tests for get by id

--- src/routes/post/__test__/get-by-id.test.ts
+++ src/routes/post/__test__/get-by-id.test.ts
@@ -1,11 +1,70 @@
 import request from 'supertest'
-import { Post } from "../../../models/posts-model";
 import { app } from '../../../app';
+import { User } from '../../../models/user-model';
 
 describe('GET /api/posts/:id', () => {
-    it.todo('fails when id is not a number')
-    it.todo('fails when no authentication header is present')
-    it.todo('fails when invalid token is sent')
-    it.todo('fails when requesting user doesn\'t own the post')
-    it.todo('successfully responds with a single post')
+    it('fails when id is not a number', async () => {
+        const param = 'kl'
+        const res = await request(app)
+          .get(`/api/posts/${param}`)
+          .send()
+          .expect(400)
+
+          expect(res.body.errors[0].message).toEqual('id must be a number as a parameter')
+    })
+
+    it('fails when no authentication header is present', async () => {
+        const res = await request(app)
+          .get('/api/posts/1')
+          .send()
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized')
+    })
+
+    it('fails when invalid token is sent', async () => {
+        const res = await request(app)
+          .get('/api/posts/1')
+          .auth('123', { type: 'bearer' })
+          .send()
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized')
+    })
+
+    it('fails when requesting user doesn\'t own the post', async () => {
+        const { id } = await createUser()
+        const otherUser = await User.create({
+          firstName: 'Some',
+          lastName: 'Guy',
+          email: 'someguy@gmail.com',
+          password: "Test123!"
+        })
+        await otherUser.save()
+
+        const token = generateToken(otherUser.toJSON().id)
+
+        const post = await createPost(id)
+
+        const res = await request(app)
+          .get(`/api/posts/${post.id}`)
+          .auth(token, { type: 'bearer' })
+          .send()
+          .expect(401)
+
+          expect(res.body.errors[0].message).toEqual('Not Authorized')
+    })
+
+    it('successfully responds with a single post', async () => {
+        const { id, token } = await createUser()
+        const post = await createPost(id)
+        const res = await request(app)
+          .get(`/api/posts/${post.id}`)
+          .auth(token, { type: 'bearer'})
+          .send()
+          .expect(200)
+
+        expect(res.body.id).toEqual(post.id)
+        expect(res.body.userId).toEqual(id)
+    })
 })
